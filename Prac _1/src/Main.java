93d1ece8fa12ce3434f8733a93ec669b36507a1b
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2024-09-10 13:36:56 +0300

    Add files via upload

---
+++ Prac _1/src/Main.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+
+
+public class Main {
+    public static int max_int(int[] array) {
+        int max_i = -2147483648;
+        for (int i : array)
+            if (i > max_i)
+                max_i = i;
+        return max_i;
+    }
+
+    public static void print_int_array(int[] array) {
+        for (int i : array)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+
+
+    public static void task_1() {
+        System.out.println("Task 1:\n");
+        Random rnd = new Random();
+        int[] array = new int[10];
+        int sumArray = 0;
+        for (int i = 0; i < array.length; i++)
+            array[i] = rnd.nextInt(100);
+        System.out.print("array: ");
+        print_int_array(array);
+
+        for (int i = 0; i < 4; i++)
+            sumArray += array[i];
+
+        float averageArray = (float) sumArray / array.length;
+        System.out.println("Sum Array: " + sumArray + "\nAverage Array: " + averageArray + "\n");
+    }
+
+    public static void task_2() {
+        System.out.println("Task 2:\n");
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[10];
+        int j = 0;
+        System.out.println("Input array:");
+        while (j < array.length) {
+            array[j] = scan.nextInt();
+            j++;
+        }
+        System.out.print("array: ");
+        print_int_array(array);
+        System.out.println("max number: " + max_int(array));
+    }
+
+//*
+    public static void task_3() {
+        float[] array = new float[10];
+        float n = 0;
+        for (float i : array) {
+            i = (float) 1 / n;
+            n++;
+        }
+    } //*/
+
+    public static void main(String[] args) {
+        task_1();
+        task_2();
+    }
+}
